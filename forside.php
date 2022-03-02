64fdb7c2c782e1c3ef699ea4cd139181fb7b1341
martin <martinreberg1@live.no>
2022-03-02 16:26:12 +0100

    -git commit

---
+++ forside.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Forside</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/forside.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<div class="container">
+    <nav id="navbar">
+        <div id="searchbar">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="text" placeholder="Hva leter du etter?">
+        </div>
+    <div id="profil">
+        <div id ="navn_poeng">
+            Veronika
+
+            125 poeng
+        </div>
+        <div id="profilbilde">
+            <a href=""><img src="assets/media/admin.png"></img></a>
+        </div>
+    </div>
+    </nav>
+    <nav id="menubar">
+        <ul>
+            <a href=""><li>Oversikt</li></a>
+            <a href=""><li>Påbegynt</li></a>
+            <a href=""><li>Fullført</li></a>
+        </ul>
+    </nav>
+    <div id="kurs_container">
+        <h1>Kom i gang med disse kursene</h1>
+        <div id="kurs1">
+            <h4>KURS</h4>
+            <img src="assets/media/lightbulb.jpg"></img>
+            <div class="bottom">
+                <p>Fra idé til konsept - vil konseptet overleve?</p>
+            </div>
+        </div>
+        <div id="kurs2">
+            <h4>KURS</h4>
+            <img src="assets/media/lightbulb.jpg"></img>
+            <div class="bottom">
+                <p>Hvor må jeg registrere bedriften min?</p>
+            </div>
+        </div>
+        <div id="kurs3">
+            <h4>KURS</h4>
+            <img src="assets/media/lightbulb.jpg"></img>
+            <div class="bottom">
+                <p>Domene - hva er det?</p>
+            </div>
+        </div>
+        <div id="kurs4">
+            <h4>KURS</h4>
+            <img src="assets/media/lightbulb.jpg"></img>
+            <div class="bottom">
+              <p>Egenkapital, investorer og regnskap - kjapt om økonomi</p>
+            </div>
+        </div>
+
+    </div>
+    <div id="kurs_container">
+            <h1>Utforsk modulene</h1>
+            <div id="modul1">
+                <h4>MODUL</h4>
+                <img src="assets/media/lightbulb.jpg"></img>
+                <div class="bottom">
+                    <p>Fra idé til konsept - vil konseptet overleve?</p>
+                </div>
+            </div>
+            <div id="modul2">
+                <h4>MODUL</h4>
+                <img src="assets/media/lightbulb.jpg"></img>
+                <div class="bottom">
+                    <p>Fra idé til konsept - vil konseptet overleve?</p>
+                </div>
+            </div>
+            <div id="modul3">
+                <h4>MODUL</h4>
+                <img src="assets/media/lightbulb.jpg"></img>
+                <div class="bottom">
+                    <p>Fra idé til konsept - vil konseptet overleve?</p>
+                </div>
+            </div>
+    </div>
+</body>
+</html>
