8696b296c3c5ef9dc1a1fd503d7596bf0017363e
andreagrimsbo <andrea@grimsbo.com>
2022-03-10 14:07:22 +0100

    Kom i gang - side 1

---
+++ komigang.php
@@ -0,0 +1,69 @@
+<!--Comitt-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Forside - Påbegynt</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/global.css">
+    <link rel="stylesheet" href="assets/css/komigang.css">
+    <script src="assets/js/effects.js"></script>
+</head>
+    <body>
+    <main class="komigang-container">
+      <div>
+        <img src="assets/images/logo.png" alt="logo-image" />
+        <nav id="menubar">
+          <div>
+            <ul>
+              <li class="login">
+                <a href="login.html" class="menulink">Logg inn</a>
+              </li>
+              <li class="about">
+                <span class="menulink">Om oss</span>
+                <ul>
+                  <li class="about-choice one hidden">Trivselslaben</li>
+                  <li class="about-choice two hidden">Inova</li>
+                  <li class="about-choice three hidden">Vår visjon</li>
+                </ul>
+              </li>
+              <li class="start"><a href="#" class="menulink">Kom i gang</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      <section>
+        <div class="content-container">
+            <article class="prisinfo">
+                <div class="anbefales"> </div>
+                <h1>Månedsbasert</h1>
+                <h2>kr 39,00/mnd</h2>
+                <ul>
+                    <li>- Full tilgang til kursmateriale</li>
+                    <li>- Over 20 ulike moduler </li>
+                    <li>- Gode repetisjonsmuligheter</li>
+                    <li>- Steg-for-steg veiledning</li>
+                </ul>
+                <button><span cass="tilgang">Få tilgang til kursmateriale</span></button>
+            </article>
+            <article class="prisinfo årlig">
+                <div class="anbefales"> <span>Anbefales</span> </div>
+                <h1>Årsbasert</h1>
+                <h2>kr 389,00/år</h2>
+                <ul>
+                    <li>- Full tilgang til kursmateriale</li>
+                    <li>- Over 20 ulike moduler </li>
+                    <li>- Gode repetisjonsmuligheter</li>
+                    <li>- Steg-for-steg veiledning</li>
+                </ul>
+                <p>Spar 15%</p>
+                <button><span cass="tilgang">Få tilgang til kursmateriale</span></button>
+            </article>
+        </div>
+      </section>
+    </main>
+    </body>
+</html>
