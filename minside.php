ec3e11a0c83f28b22aad1f1d9cc47701a791154a
andreagrimsbo <andrea@grimsbo.com>
2022-03-21 15:28:10 +0100

    min side - administrer personlig info

---
+++ minside.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/reset.css"/>
+    <link rel="stylesheet" href="assets/css/global.css"/>
+    <link rel="stylesheet" href="assets/css/minside.css">
+</head>
+    <body id="body">
+        <?php
+            require "assets/reuse/navbar.php";
+        ?>
+        <main>
+            <nav id="admin-nav">
+                <h1>Min profil</h1>
+                <ul>
+                    <li>Brukerinformasjon</li>
+                    <li>Mine interesser</li>
+                    <li>Lagrede kurs</li>
+                </ul>
+            </nav>
+            <section>
+                <!-- Oppdater brukerinfo --> 
+                <form action="">
+                    <div>
+                        <!-- #TODO Sett inn bilde/last opp nytt - fikser css senere -->
+
+                        <!-- #TODO Sett inn navn på bruker her -->
+                        <h2>Veronika Hansen</h2>
+                    </div>
+
+                    <div>
+                        <!-- #TODO??? Sette inn fornavn, etternavn og e-post i placeholderne?? -->
+                        <label for="nyttfnavn">Fornavn</label>
+                        <input name="nyttfnavn" type="text" id="nyttfnavn" placeholder="fornavn">
+
+                        <label for="nyttenavn">Etternavn</label>
+                        <input name="nytteenavn" type="text" id="nyttenavn" placeholder="etternavn">
+                    </div>
+
+                    <div>
+                        <label for="nyepost">E-post</label>
+                        <input name="nyepost" type="text" id="nyepost" placeholder="e-post">
+
+                        <label for="nyepostconf">Bekreft e-post</label>
+                        <input name="nyepostconf" type="text" id="nyepostconf" placeholder="bekreft epost">
+                    </div>
+
+                    <div>
+                        <button type="submit">Registrer</button>
+                    </div>
+                </form>
+            </section>
+
+        </main>
+    </body>
+</html>
