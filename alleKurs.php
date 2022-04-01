b491285304c490431f103279caf2f69933be4d31
andreagrimsbo <andrea@grimsbo.com>
2022-04-01 15:23:40 +0200

    alle moduler/kurs

---
+++ alleKurs.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Forside - Oversikt</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/common/reset.css">
+    <link rel="stylesheet" href="assets/css/common/global.css">
+    <link rel="stylesheet" href="assets/css/common/header.css">
+    <link rel="stylesheet" href="assets/css/common/top-nav.css">
+    <link rel="stylesheet" href="assets/css/forside.css">
+    <link rel="stylesheet" href="assets/css/common/alle-kurs-moduler.css">
+    <script src="assets/js/top-nav.js"></script>
+</head>
+
+<body>
+    <header>
+        <?php
+            require "assets/reuse/navbar.php";
+            require "assets/reuse/top-nav.php"; 
+        ?>
+    </header>
+
+    <main>
+        <?php require "assets/reuse/filter.php"?>
+        
+        <div id="alle-kurs">
+            <?php require "assets/reuse/hentKurs.php"; ?>
+        </div>
+    </main>
+
+</body>
+</html>
