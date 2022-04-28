6b28d26ed932bb547f8f19eb58caa3383a3f9a23
martin <martinreberg1@live.no>
2022-04-28 21:44:19 +0200

    Henter nå ut moduler også i fullført

--- assets/reuse/hentFullført.php
+++ assets/reuse/hentFullført.php
@@ -4,34 +4,88 @@ $id = $_SESSION['id'];
 $sql = $conn->prepare("SELECT * FROM person_i_quiz pq, quiz q WHERE pq.quiz_id = q.quiz_id AND pq.person_id = $id AND pq.antall_svart >= q.antall_spørsmål");
 $sql->execute();
 $result = $sql->get_result();
+$sql1 = $conn->prepare("SELECT * FROM person_i_modul pm, modul m WHERE pm.modul_id = m.modul_id AND pm.person_id = $id");
+$sql1->execute();
+$result1 = $sql1->get_result();
 if ($result->num_rows > 0) {
-    echo '<h2>Her finner du alt du har fullført </h2>';
+    $tittel = "<h2>Her finner du alt du har fullført </h2>";
+    echo $tittel;
     while($row = $result->fetch_assoc()) {
-
-        $prosent = $row['antall_svart'] / $row['antall_spørsmål'] * 100;
+            $prosent = $row['antall_rette'] / $row['antall_spørsmål'] * 100;
+            $prosentdesimal = $prosent/100;
+            $grader = 360 * $prosentdesimal / 2;
+            $dato = date_create($row['fullført_dato']);
+            $nyDato = date_format($dato, "d.m.Y");
+            echo '<div class="fullført">';
+            echo '<h4>QUIZ</h4>';
+            echo '<div class="circle-wrap">';
+            echo '<div class="circle">';
+            echo '<div class="mask full" style="animation: fill';
+            echo $row['quiz_id'];
+            echo ' ease-in-out 2s; transform: rotate(';
+            echo '180';
+            echo 'deg);';
+            echo '">';
+            echo '<div class="fill" style="animation: fill';
+            echo $row['quiz_id'];
+            echo ' ease-in-out 2s; transform: rotate(';
+            echo '180';
+            echo 'deg);';
+            echo '"></div></div>';
+            echo '<div class="mask half">';
+            echo '<div class="fill" style="animation: fill';
+            echo $row['quiz_id'];
+            echo ' ease-in-out 2s; transform: rotate(';
+            echo '180';
+            echo 'deg);';
+            echo '"></div>';
+            echo '</div><div class="inside-circle">100%</div>';
+            echo '</div></div>';
+            echo '<div class="poeng"><p>Opptjente poeng: <span>';
+            echo $row['quizpoeng'];
+            echo '</span></p><p>Fullført: <span>';
+            echo $nyDato;
+            echo '</span></p></div><div class="bottom"><h3>';
+            echo $row['quiznavn'];
+            echo '</h3>';
+            echo '<style>';
+            echo '@keyframes fill';
+            echo $row['quiz_id'];
+            echo '{0% {transform: rotate(0deg);}100% {transform: rotate(';
+            echo '180';
+            echo 'deg);}}';
+            echo '</style></div></div>';
+        }
+} if ($result1->num_rows > 0) {
+    if (!isset($tittel)) {
+        $tittel = "<h2>Her finner du alt du har fullført </h2>";
+            echo $tittel;
+    }
+     while($row1 = $result1->fetch_assoc()) {
+        $prosent = 100;
         $prosentdesimal = $prosent/100;
         $grader = 360 * $prosentdesimal / 2;
-        $dato = date_create($row['fullført_dato']);
+        $dato = date_create($row1['fullført_dato']);
         $nyDato = date_format($dato, "d.m.Y");
         echo '<div class="fullført">';
-        echo '<h4>QUIZ</h4>';
+        echo '<h4>MODUL</h4>';
         echo '<div class="circle-wrap">';
         echo '<div class="circle">';
         echo '<div class="mask full" style="animation: fill';
-        echo $row['quiz_id'];
+        echo $row1['modul_id'];
         echo ' ease-in-out 2s; transform: rotate(';
         echo '180';
         echo 'deg);';
         echo '">';
         echo '<div class="fill" style="animation: fill';
-        echo $row['quiz_id'];
+        echo $row1['modul_id'];
         echo ' ease-in-out 2s; transform: rotate(';
         echo '180';
         echo 'deg);';
         echo '"></div></div>';
         echo '<div class="mask half">';
         echo '<div class="fill" style="animation: fill';
-        echo $row['quiz_id'];
+        echo $row1['modul_id'];
         echo ' ease-in-out 2s; transform: rotate(';
         echo '180';
         echo 'deg);';
@@ -39,21 +93,21 @@ if ($result->num_rows > 0) {
         echo '</div><div class="inside-circle">100%</div>';
         echo '</div></div>';
         echo '<div class="poeng"><p>Opptjente poeng: <span>';
-        echo $row['quizpoeng'];
+        echo $row1['modul_poeng'];
         echo '</span></p><p>Fullført: <span>';
         echo $nyDato;
         echo '</span></p></div><div class="bottom"><h3>';
-        echo $row['quiznavn'];
+        echo $row1['navn'];
         echo '</h3>';
         echo '<style>';
         echo '@keyframes fill';
-        echo $row['quiz_id'];
+        echo $row1['modul_id'];
         echo '{0% {transform: rotate(0deg);}100% {transform: rotate(';
         echo '180';
         echo 'deg);}}';
-        echo '</style>';
-
+        echo '</style></div></div>';
     }
 } else {
-    echo '<h2>Det ser ikke ut som du har fullført noe. Trykk nedenfor for å begynne<br><br><br><a href="alleQuizer.php" class="utfclass">Test deg selv i quiz</a><br><br><a href="alleKurs.php" class="utfclass">Finn et kurs</a><br><br><a href="alleModuler.php" class="utfclass">Utforsk moduler</a></h2>';
+    $tittel = '<h2>Det ser ikke ut som du har fullført noe. Trykk nedenfor for å begynne<br><br><br><a href="alleQuizer.php" class="utfclass">Test deg selv i quiz</a><br><br><a href="alleKurs.php" class="utfclass">Finn et kurs</a><br><br><a href="alleModuler.php" class="utfclass">Utforsk moduler</a></h2>';
+    echo $tittel;
 }
