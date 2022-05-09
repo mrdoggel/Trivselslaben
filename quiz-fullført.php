1c0014d1f7befa2aee3947c414737f594f716b00
Shrekmeister360 <hauge_karina@outlook.com>
2022-05-09 12:12:20 +0200

    @media på modul.

--- quiz-fullført.php
+++ quiz-fullført.php
@@ -22,11 +22,8 @@
         <div class="quiz-res-container">
             <h2>Du har nå fullført quizen</h2> 
            <?php require "assets/reuse/hentQuizResultat.php"; ?>
-
         </div>
-
         <p><span><a href="alleQuizer.php">Utforsk flere quizer</a></span></p>
-        
     </main>
 </body>
 </html>
